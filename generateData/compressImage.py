177c82c15fc2fbac7f14f24e2206cbec79121c91
Akshay Agarwal <mathgeek145@gmail.com>
2021-06-21 00:08:41 +0800

    all_data_compressed

--- generateData/compressImage.py
+++ generateData/compressImage.py
@@ -3,13 +3,77 @@ import os
 import PIL 
 import glob 
 
+os.chdir('/home/mc145/Programming/BoardToFen/generateData/rawdata/pawn') 
 
-image = Image.open('rawdata/whitePawns/0001whitepawn .png')
+counter = 1
 
-resized_image = image.resize((64,64)) 
-print(resized_image.size)
+for img in os.listdir():
+    image = Image.open(img) 
+    resized_image = image.resize((64,64))
+    print(resized_image.size) 
+    resized_image = resized_image.save(f'/home/mc145/Programming/BoardToFen/training/data/pawn/{counter}compressedpawn.png')  
+    counter += 1
 
-resized_image = resized_image.save("test.png") 
 
+os.chdir('/home/mc145/Programming/BoardToFen/generateData/rawdata/knight') 
+
+counter = 1
+
+for img in os.listdir():
+    image = Image.open(img) 
+    resized_image = image.resize((64,64))
+    print(resized_image.size) 
+    resized_image = resized_image.save(f'/home/mc145/Programming/BoardToFen/training/data/knight/{counter}compressedknight.png')  
+    counter += 1
+
+os.chdir('/home/mc145/Programming/BoardToFen/generateData/rawdata/bishop') 
+
+counter = 1
+
+for img in os.listdir():
+    image = Image.open(img) 
+    resized_image = image.resize((64,64))
+    print(resized_image.size) 
+    resized_image = resized_image.save(f'/home/mc145/Programming/BoardToFen/training/data/bishop/{counter}compressedbishop.png')  
+    counter += 1
+
+
+os.chdir('/home/mc145/Programming/BoardToFen/generateData/rawdata/rook') 
+
+counter = 1
+
+for img in os.listdir():
+    image = Image.open(img) 
+    resized_image = image.resize((64,64))
+    print(resized_image.size) 
+    resized_image = resized_image.save(f'/home/mc145/Programming/BoardToFen/training/data/rook/{counter}compressedrook.png')  
+    counter += 1
+
+
+
+
+
+os.chdir('/home/mc145/Programming/BoardToFen/generateData/rawdata/queen') 
+
+counter = 1
+
+for img in os.listdir():
+    image = Image.open(img) 
+    resized_image = image.resize((64,64))
+    print(resized_image.size) 
+    resized_image = resized_image.save(f'/home/mc145/Programming/BoardToFen/training/data/queen/{counter}compressedqueen.png')  
+    counter += 1
+
+
+os.chdir('/home/mc145/Programming/BoardToFen/generateData/rawdata/king') 
+
+counter = 1
+
+for img in os.listdir():
+    image = Image.open(img) 
+    resized_image = image.resize((64,64))
+    print(resized_image.size) 
+    resized_image = resized_image.save(f'/home/mc145/Programming/BoardToFen/training/data/king/{counter}compressedking.png')  
+    counter += 1
 
 
